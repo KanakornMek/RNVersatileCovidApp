25020318c8d5e4c25426eb80e10b5abcea4bf74c
Ttouch-thareewet <92733491+Ttouch-thareewet@users.noreply.github.com>
2021-12-03 17:28:03 +0700

    create button chat bot and resource
    create button chat bot and resource

--- src/screen/otherfunctionscreen/maino.function.js
+++ src/screen/otherfunctionscreen/maino.function.js
@@ -1,22 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View, Button, Image } from 'react-native';
 
-export default function Chatbot () {
-    return(
-        <View>
-            <Text style={styles.testText} >Chatbot</Text>
-            <Text style={{fontSize: 40, color: 'black'}} >Chatbot</Text>
-            <Text style={{fontSize: 40, color: 'black', fontFamily: 'Prompt-Bold'}}>ภาษาไทย</Text>
-            <Text style={{fontSize: 40}}>ภาษาไทย</Text>
+export default function Chatbot() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.wrapper} >
+                <Pressable style={styles.imageContainer}>
+                    <Image resizeMode='contain' style={{ flex: 1 }} source={require('../../../assets/chatbot.png')} />
+                </Pressable>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text1}>
+                        เเชทบอท
+
+                    </Text>
+                    <Text style={styles.text2}>
+                        ใช้แชทบอทเพื่อการ
+
+                    </Text>
+                    <Text style={styles.text2}>
+
+                        เข้าถึงข้อมูลที่ง่ายกว่า
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.wrapper} >
+                <View style={styles.textContainer}>
+                    <Text style={styles.text1}>
+                    คลังข้อมูล    
+                    </Text>
+                    <Text style={styles.text2}>
+                    เข้าถึงข้อมูลเกี่ยวกับโควิด-19อย่างรวดเร็ว
+
+                    </Text>
+                    <Text style={styles.text2}>
+
+                        เข้าถึงข้อมูลที่ง่ายกว่า
+                    </Text>
+                </View>
+                <Pressable style={styles.imageContainer}>
+                    <Image resizeMode='contain' style={{ flex: 1 }} source={require('../../../assets/resource.png')} />
+                </Pressable>
+
+            </View>
         </View>
+
+
     );
 }
 
 const styles = StyleSheet.create({
-    testText: {
-        fontSize: 40,
-        color: 'black',
-        fontFamily: 'Roboto-Regular',
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    textContainer: {
+        flex: 1,
+        justifyContent : 'center',
+        padding :10,
         
-     }
+    },
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding : 10,
+    },
+    wrapper: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 10,
+    },
+    text1: {
+        fontWeight: 'bold',
+        fontSize: 39,
+        color: 'black',
+    },
+    text2: {
+        fontSize: 20,
+        color: 'black',
+    },
 });
