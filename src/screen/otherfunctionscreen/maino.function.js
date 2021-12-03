ea626f34d46faf817ec7b65293b76a5d8afa9d54
a <a@Macintosh.local>
2021-12-03 20:25:59 +0700

    edit padding text in chatbot main page

--- src/screen/otherfunctionscreen/maino.function.js
+++ src/screen/otherfunctionscreen/maino.function.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Pressable, StyleSheet, Text, View, Button, Image} from 'react-native';
 
-
 export default function Chatbot() {
   return (
     <View style={styles.container}>
@@ -9,32 +8,48 @@ export default function Chatbot() {
         <Pressable style={styles.imageContainer}>
           <Image
             resizeMode="contain"
-            style={{flex: 1}}
+            style={{width: '100%'}}
             source={require('../../../assets/chatbot.png')}
           />
         </Pressable>
         <View style={styles.textContainer}>
-          <Text numberOfLines={1} adjustsFontSizeToFit style={styles.text1}>
-            เเชทบอท
-          </Text>
-          <Text adjustsFontSizeToFit style={styles.text2}>
-            ใช้แชทบอทเพื่อการเข้าถึงข้อมูลที่ง่ายกว่า
-          </Text>
+          <View
+            style={{
+              width: '80%',
+              height: '80%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text numberOfLines={1} adjustsFontSizeToFit style={styles.text1}>
+              เเชทบอท
+            </Text>
+            <Text adjustsFontSizeToFit style={styles.text2}>
+              ใช้แชทบอทเพื่อการเข้าถึงข้อมูลที่ง่ายกว่า
+            </Text>
+          </View>
         </View>
       </View>
       <View style={styles.wrapper}>
         <View style={styles.textContainer}>
-          <Text numberOfLines={1} adjustsFontSizeToFit style={styles.text1}>
-            คลังข้อมูล
-          </Text>
-          <Text adjustsFontSizeToFit style={styles.text2}>
-            เข้าถึงข้อมูลเกี่ยวกับโควิด-19 อย่างรวดเร็วเข้าถึงข้อมูลที่ง่ายกว่า
-          </Text>
+          <View
+            style={{
+              width: '80%',
+              height: '80%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text numberOfLines={1} adjustsFontSizeToFit style={styles.text1}>
+              คลังข้อมูล
+            </Text>
+            <Text adjustsFontSizeToFit style={styles.text2}>
+              เข้าถึงข้อมูลเกี่ยวกับโควิด-19 อย่างรวดเร็วเข้าถึงข้อมูลที่ง่ายกว่า
+            </Text>
+          </View>
         </View>
         <Pressable style={styles.imageContainer}>
           <Image
             resizeMode="contain"
-            style={{flex: 1}}
+            style={{width: '100%'}}
             source={require('../../../assets/resource.png')}
           />
         </Pressable>
@@ -47,22 +62,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    padding: 10,
   },
   textContainer: {
     flex: 1,
     justifyContent: 'center',
-    padding: 10,
+    alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10,
   },
   wrapper: {
     flex: 1,
     flexDirection: 'row',
-    margin: 10,
   },
   text1: {
     fontWeight: 'bold',
@@ -74,4 +88,3 @@ const styles = StyleSheet.create({
     color: 'black',
   },
 });
-
